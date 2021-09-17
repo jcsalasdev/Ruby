854cd3a3f605a881b5b56e5fd40c1443c80ae751
jcsalasdev <jcsaalasdev@gmail.com>
2021-09-17 22:59:54 +0800

    act2

---
+++ activity2.rb
@@ -0,0 +1,39 @@
+#1
+arr = [1, 3, 5, 7, 9, 11]
+num = 3
+if arr.each { |number| number == num }
+    puts "#{num} is found"
+end
+
+#2
+puts "please enter a number 1-100"
+numbers = gets.chop.to_i
+
+if numbers <= 50 
+    puts " The number scope of #{numbers} is 0-50"
+
+elsif numbers > 50 && numbers == 100
+    puts "The number scope of #{numbers} is 51-100"
+
+else 
+   puts "The number scope of #{numbers} is above 100"
+end
+
+#3
+while true
+    puts "   Enter you name"
+    name = gets.chomp
+    25.times{print "-"}
+    puts ""
+    puts "   type STOP to quit"
+    25.times{print "-"}
+    puts ""
+    input = gets.chomp
+    break if input == "STOP"
+end
+
+#4
+arr2 = [6, 3, 1, 8, 4, 2, 10, 65, 102]
+arr2.each do |number|
+    puts number if number % 2 == 0;
+end
