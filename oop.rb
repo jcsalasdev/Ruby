23e3fc0a74dc34644c08ff1969c1c024064efe84
jcsalasdev <jcsaalasdev@gmail.com>
2021-09-18 10:56:01 +0800

    accessor

--- oop.rb
+++ oop.rb
@@ -15,3 +15,26 @@ class User
   user.run
   User.identify_yourself
   puts "name: #{user.name} | email: #{user.email}"
+
+35.times{print "-"}
+puts " "
+
+  class Employee
+    attr_accessor :first_name, :last_name, :email
+    def initialize(firstname, lastname, email)
+      @first_name = firstname
+      @last_name = lastname
+      @email = email
+
+    end
+   
+    def to_s
+      "First name: #{@first_name}, Last name: #{@last_name}, email address: #{@email}"
+    end
+   
+  end
+   
+  jcdev = Employee.new("Jcdev", "Salas", "jcsalasdev@example.com" )
+  abdul = Employee.new("Abdul", "Magundacan", "Abdul@example.com" )
+  puts jcdev
+  puts abdul
