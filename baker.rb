e3b52b3438d49b303e3b6c110c1eb2ca639b8b55
jcsalasdev <jcsaalasdev@gmail.com>
2021-09-20 19:50:04 +0800

    baker

--- baker.rb
+++ baker.rb
@@ -2,36 +2,26 @@ class Confection
     attr_accessor :cake
     def initialize(cake)
       @cake = cake
-
     end
 
     def baking
-        puts "#{cake} is baking at 350 degrees for 25 minutes."
+        40.times{print "-"}
+        puts " "
+        puts "#{@cake} is baking at 350 degrees for 25 minutes."
     end
-
 end
 
 class Cupcake < Confection  
     def baking
         super
-         puts "applying frosting at #{@cake}"
+         puts "Apply frosting at #{@cake}"
     end
 
+    cupCake = Cupcake.new("Cup Cake")
+    cupCake.baking
 end
-
-35.times{print "-"}
-puts " "
-
-cupCake = Cupcake.new("Cup Cake")
-cupCake.baking
-
-35.times{print "-"}
-puts " "
 
 class BananaCake < Confection
-
+    bananaCake = BananaCake.new("Banana Cake")
+    bananaCake.baking
 end
-
-bananaCake = BananaCake.new("Banana Cake")
-bananaCake.baking
-
--- min_num.rb
+++ min_num.rb
@@ -2,7 +2,7 @@ n = [4, 15, -8, -2, 25]
 
 def min_num(n)
     minimum = n[0]
-
+    
     n.each  do |element|
         if element < minimum
         minimum = element
